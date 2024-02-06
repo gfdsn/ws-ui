ee223e3d36831a93a2681286885cce89e5b28681
Guilherme Santos <guilhermefsantosn@gmail.com>
2024-02-06 01:54:18 +0000

    feat: previous messages handling

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import Header from '../Header';
 import useWebSocket from 'react-use-websocket';
 import { Button } from 'react-bootstrap';
+
+interface Message {
+  _id: string,
+  authorId: string,
+  roomId: string,
+  message: string
+}
 
 export default function Room() {
 
@@ -10,20 +17,39 @@ export default function Room() {
   const roomId = params.id;
 
   const [msg, setMsg] = useState("");
-  const {sendMessage} = useWebSocket(`ws://localhost:8080/chatapp/room/${roomId}`, {
-    onOpen: () => {console.log("connected"); sendMessage("conectei fi")},
-    onMessage: (message: WebSocketEventMap['message']) => console.log(message.data),
-    onClose: () => sendMessage("vazei fi"),
-    onError: error => console.log("WebSocket Error:", error),
-  })
+  const [messages, setMessages] = useState<Message[]>([])
+  const [previousMessagesRecieved, setPreviousMessagesRecieved] = useState<boolean>(false);
 
-  const sendMsg = () => {
-    sendMessage(msg)
-  }
+  const { sendMessage } = useWebSocket(`ws://localhost:8080/chatapp/room/${roomId}`, {
+    onOpen: () => { console.log("connected");   },
+    onMessage: (message: WebSocketEventMap['message']) => {
+      const roomMessages = JSON.parse(message.data);
+
+      /* TO-DO fix this */
+      if (!previousMessagesRecieved) {
+        setMessages(prevMessages => [...prevMessages, ...roomMessages]); 
+        setPreviousMessagesRecieved(true)
+      } else setMessages(prevMessages => [...prevMessages, roomMessages]) 
+
+    },
+    onError: error => { console.log("WebSocket Error:", error); },
+  });
+  
+  const sendMsg = () => sendMessage(msg)
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
 
   return (
     <>
       <Header roomId={Number(roomId)}/>
+      {
+        messages &&
+        messages.map(message => (
+          <p>{message.message}</p>
+        ))
+      }
       <input type='text' id='message' onChange={event => setMsg(event.target.value)} />
       <Button onClick={sendMsg}>Send message</Button>
     </>
