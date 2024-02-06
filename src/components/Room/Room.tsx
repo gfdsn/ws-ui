946c5f58a1e947a75c22d4d7099ba330c6c80f35
Guilherme Santos <guilhermefsantosn@gmail.com>
2024-02-06 22:41:32 +0000

    fix: fixed previous messages

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,57 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import Header from '../Header';
 import useWebSocket from 'react-use-websocket';
 import { Button } from 'react-bootstrap';
 
 interface Message {
-  _id: string,
-  authorId: string,
-  roomId: string,
-  message: string
+  _id: string;
+  authorId: string;
+  roomId: string;
+  message: string;
 }
 
 export default function Room() {
+  const { id: roomId } = useParams<{ id: string }>();
 
-  const params = useParams()
-  const roomId = params.id;
-
-  const [msg, setMsg] = useState("");
-  const [messages, setMessages] = useState<Message[]>([])
-  const [previousMessagesRecieved, setPreviousMessagesRecieved] = useState<boolean>(false);
+  const [msg, setMsg] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [previousMessagesReceived, setPreviousMessagesReceived] = useState<boolean>(false);
 
   const { sendMessage } = useWebSocket(`ws://localhost:8080/chatapp/room/${roomId}`, {
-    onOpen: () => { console.log("connected");   },
+    onOpen: () => console.log('connected'),
     onMessage: (message: WebSocketEventMap['message']) => {
       const roomMessages = JSON.parse(message.data);
 
-      /* TO-DO fix this */
-      if (!previousMessagesRecieved) {
-        setMessages(prevMessages => [...prevMessages, ...roomMessages]); 
-        setPreviousMessagesRecieved(true)
-      } else setMessages(prevMessages => [...prevMessages, roomMessages]) 
+      if (!previousMessagesReceived){
+        setMessages(prevMessages => [...prevMessages, ...roomMessages]);
+        setPreviousMessagesReceived(true);
+
+      } else setMessages(prevMessages => [...prevMessages, roomMessages]);
 
     },
-    onError: error => { console.log("WebSocket Error:", error); },
+    onError: error => console.log('WebSocket Error:', error),
   });
-  
-  const sendMsg = () => sendMessage(msg)
 
-  useEffect(() => {
-    console.log(messages);
-  }, [messages]);
+  const sendMsg = () => {
+    sendMessage(msg);
+    setMsg('');
+  };
 
   return (
     <>
-      <Header roomId={Number(roomId)}/>
-      {
-        messages &&
-        messages.map(message => (
-          <p>{message.message}</p>
-        ))
-      }
-      <input type='text' id='message' onChange={event => setMsg(event.target.value)} />
-      <Button onClick={sendMsg}>Send message</Button>
+      <Header roomId={Number(roomId)} />
+      {messages.map((message, index) => (
+        <p key={index}>{message.message}</p>
+      ))}
+      <input type="text" id="message" value={msg} onChange={event => setMsg(event.target.value)} />
+      <Button onClick={sendMsg}>Send messages</Button>
     </>
-  )
+  );
 }
